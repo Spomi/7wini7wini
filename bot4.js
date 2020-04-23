4689331e6f35900d4d647a8ae2dac87340824428
Spomi <40448323+Spomi@users.noreply.github.com>
2020-04-23 20:02:18 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -1,31 +1,10 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '='
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`SPACE AGAR.IO`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
 
 
@@ -77,4 +56,4 @@ client.channels.find('id', '668112223490539521').setName("🔊announcement🕪")
 
 
 
-client.login(process.env.BOT4_TOKEN);
+client.login("BOT4_TOKEN");
