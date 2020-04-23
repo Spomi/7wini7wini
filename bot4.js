df0432f9960abda2fcc68d040d2c4c3971d21789
Spomi <40448323+Spomi@users.noreply.github.com>
2020-04-23 21:39:34 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -12,7 +12,27 @@ client.on('ready', () => {
 
 
 
-
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '453449558761144330').setName("W");
+client.channels.find('id', '453449558761144330').setName("We");
+client.channels.find('id', '453449558761144330').setName("Wel");
+client.channels.find('id', '453449558761144330').setName("Welc");
+client.channels.find('id', '453449558761144330').setName("Welco");
+client.channels.find('id', '453449558761144330').setName("Welcom");
+client.channels.find('id', '453449558761144330').setName("Welcome");
+client.channels.find('id', '453449558761144330').setName("Welcome T");
+client.channels.find('id', '453449558761144330').setName("Welcome To");
+client.channels.find('id', '453449558761144330').setName("Welcome To ");
+client.channels.find('id', '453449558761144330').setName("Welcome To S");
+client.channels.find('id', '453449558761144330').setName("Welcome To SE");
+client.channels.find('id', '453449558761144330').setName("Welcome To SER");
+client.channels.find('id', '453449558761144330').setName("Welcome To SERV");
+client.channels.find('id', '453449558761144330').setName("Welcome To SERVE");
+client.channels.find('id', '453449558761144330').setName("Welcome To SERVER");
+}, 8000);
+ 
+});
 
 
 
