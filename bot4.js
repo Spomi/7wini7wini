dbd256b8c9c0ad2e2bc3b7caf664c023bbbedf02
Spomi <40448323+Spomi@users.noreply.github.com>
2020-04-24 13:41:34 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -1,10 +1,32 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '+'
 
-
-
+   
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+client.on('ready', function(){
+    var ms = 5000 ;
+    var setGame = [`${prefix}help Servers ${client.guilds.size} `,`${prefix}invite Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/spomi_yt`);
+    }, ms);9000
+
 });
 
 
@@ -44,4 +66,4 @@ client.channels.find('id', '668112223490539521').setName("🔊announcement🕪")
 
 
 
-client.login("BOT4_TOKEN");
+client.login(process.env.BOT4_TOKEN);
