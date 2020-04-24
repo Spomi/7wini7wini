7e1a6e34ac232a3df223ca2411da4f3c4fb5a1fa
Spomi <40448323+Spomi@users.noreply.github.com>
2020-04-24 13:12:24 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -11,30 +11,6 @@ client.on('ready', () => {
 
 
 
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '668112223490539521').setName("🔊");
-client.channels.find('id', '668112223490539521').setName("🔊a");
-client.channels.find('id', '668112223490539521').setName("🔊an");
-client.channels.find('id', '668112223490539521').setName("🔊ann");
-client.channels.find('id', '668112223490539521').setName("🔊anno");
-client.channels.find('id', '668112223490539521').setName("🔊annou");
-client.channels.find('id', '668112223490539521').setName("🔊announ");
-client.channels.find('id', '668112223490539521').setName("🔊announc");
-client.channels.find('id', '668112223490539521').setName("🔊announce");
-client.channels.find('id', '668112223490539521').setName("🔊announcem");
-client.channels.find('id', '668112223490539521').setName("🔊announceme");
-client.channels.find('id', '668112223490539521').setName("🔊announcemen");
-client.channels.find('id', '668112223490539521').setName("🔊announcement");
-client.channels.find('id', '668112223490539521').setName("🔊announcement🕪");
-}, 8000);
- 
-});
-
-
-
-
- 
 
 
 
