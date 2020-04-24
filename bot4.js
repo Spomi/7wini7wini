8d17e08d71d89f0c4b2f38b3a6c41495109da338
Spomi <40448323+Spomi@users.noreply.github.com>
2020-04-24 15:21:58 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -54,7 +54,22 @@ client.on('message', message => {
 
  
 
+client.on('message', message => {
+var prefix = "+";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+ 
 
 
 
