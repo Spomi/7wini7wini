0a989c9ff578d5d496f41dbffe3b9006d5dbf9b0
Spomi <40448323+Spomi@users.noreply.github.com>
2020-06-09 02:17:21 +0000

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -206,7 +206,7 @@ message.channel.send({embed});
 
 
 client.on('message', message => {
-       if (message.content.startsWith(prefix + 'botinfo')) {
+       if (message.content.startsWith(prefix + 'botinfoadmin')) {
      let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
   let embed = new Discord.RichEmbed()
   .setTitle(`${client.guilds.size}سيرفرات `)
